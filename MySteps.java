9d3350adbbfa84f988b479873621aa25657c83dc
Nicky <145572780+NicoleTurksma@users.noreply.github.com>
2023-11-19 22:21:59 +0100

    Add files via upload

---
+++ MySteps.java
@@ -0,0 +1,48 @@
+package org.example.tests;
+
+import io.cucumber.java.After;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MySteps {
+
+    WebDriver webDriver = new FirefoxDriver();
+
+    @Given("I am on the movie database website")
+    public void on_the_website() {
+        // Write code here that turns the phrase above into concrete actions
+        webDriver.get("https://www.themoviedb.org/");
+    }
+    @Given("I search for Titanic")
+    public void cursor_Titanic() {
+        // Zoek het input-element op basis van de ID
+        WebElement searchInput = webDriver.findElement(By.cssSelector("input#inner_search_v4"));
+
+// Voer tekst in het zoekveld in
+        searchInput.sendKeys("Titanic");
+    }
+
+    @When("I click the search button")
+    public void I_click_the_search_button() {
+        // Write code here that turns the phrase above into concrete actions
+        WebElement searchButton = webDriver.findElement(By.cssSelector("input[type='submit'][value='Search']"));
+        searchButton.click();
+    }
+
+    @Then("The first search result should be Titanic")
+    public void first_search_should_be_Titanic() {
+        // Write code here that turns the phrase above into concrete actions
+        WebElement firstSearch = webDriver.findElement(By.xpath("//h2[contains(text(),'Titanic')]"));
+    }
+
+    @After // runs after all tests, we clean up stuff here
+    public void CleanUp(){
+        //always close your web driver
+        webDriver.close();
+    }
+}
